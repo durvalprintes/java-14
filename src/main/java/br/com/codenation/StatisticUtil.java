95ba36f798f4edf237b93c2e7469c65aad425763
Durval Printes <durvalprintes@gmail.com>
2020-02-21 10:43:21 -0300

    Desafio finalizado

--- src/main/java/br/com/codenation/StatisticUtil.java
+++ src/main/java/br/com/codenation/StatisticUtil.java
@@ -1,16 +1,39 @@
 package br.com.codenation;
 
+import java.util.Arrays;
+
 public class StatisticUtil {
 
 	public static int average(int[] elements) {
-		return 0;
+		return Math.round(Arrays.stream(elements).reduce(0, Integer::sum)/elements.length);
 	}
 
 	public static int mode(int[] elements) {
-		return 0;
+		int[] array = Arrays.stream(elements).sorted().toArray();
+		int element = array[0], count = 1, 
+			highElement = element, highCount = 0;
+		for (int i = 1; i < array.length; i++) {
+			if (element == array[i] && i < array.length - 1) {
+				count++;
+			} else {
+				if (count >= highCount) {
+					highElement = element;
+					highCount = count;
+				}
+				element = array[i];
+				count = 1;
+			}
+		}
+		return highElement;
 	}
 
 	public static int median(int[] elements) {
-		return 0;
+		int[] array = Arrays.stream(elements).sorted().toArray();
+		int size = array.length;
+		if (size % 2 == 1) {
+			return array[Math.floorDiv(size, 2)];
+		} else {
+			return Math.round((array[size/2] + array[(size/2)-1]) / 2);
+		}		
 	}
 }
